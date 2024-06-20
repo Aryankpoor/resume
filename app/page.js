7d7c0839655363b631c4b601daae7c88ebe07020
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-20 23:47:59 +0530

    NEED TO REVERT CHANGES IMMIDIETLY

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-
+import image from 'next/image'
 
 export default function Home() {
   return (
     <div className="bg-blue-900 w-screen h-screen flex items-center">
-      <div>
-      <button className="bg-white p-2">Login with Google</button>
+      <div className="text-center w-full">
+      <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
       </div> 
     </div>
   )
