71bed95caa0f1c0fffa399c2c594442eb12cfa02
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-22 12:45:24 +0530

    Edited layout page and added login plugin

--- admin-panel/src/app/admin/layout.tsx
+++ admin-panel/src/app/admin/layout.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+"use client"
+import { useAppSelector } from '@/redux/hooks';
+import Login from "@/components/admin-apnel/login";
+import React from 'react';
+import { useSession }  from "next-auth/react";
+
 
 const layout = () => {
+
+  const isLoading = useAppSelector(store => store.LoadingReducer);
+  const {data: session} = useSession();
+
+  if (!session?.user){
+    return <Login />;
+  }
+
   return (
     <div>
       layout
     </div>
   )
-}
+};
 
-export default layout
+export default layout;
