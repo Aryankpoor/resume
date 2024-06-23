75fbb36233ce7d1828c89ab6e6aa4987c68041a3
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-23 17:04:05 +0530

    added navbar components

--- frontend/ecommerce-site/src/components/navbar/navbar.jsx
+++ frontend/ecommerce-site/src/components/navbar/navbar.jsx
@@ -11,13 +11,18 @@ const navbar = () => {
         <p>Shopper</p>
       </div>
       <ul className="nav-menu">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
       </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="cart" />
+      </div>
     </div>
   )
 }
 
 export default navbar
+ 
