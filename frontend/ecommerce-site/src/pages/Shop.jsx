e49ede7441084c37106b623734581d6a5b9a1883
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-23 23:12:49 +0530

    ADDED POPULAR SECTION

--- frontend/ecommerce-site/src/pages/Shop.jsx
+++ frontend/ecommerce-site/src/pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero/hero';
+import popular from '../components/popular/popular';
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <popular />
     </div>
   )
 }
