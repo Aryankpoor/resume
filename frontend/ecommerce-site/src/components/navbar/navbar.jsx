6b7f3fdc0db5f95fc50e9ed1c05a86c20e33e770
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-23 16:36:50 +0530

    Added assets and import cart and logo

--- frontend/ecommerce-site/src/components/navbar/navbar.jsx
+++ frontend/ecommerce-site/src/components/navbar/navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './navbar.css'
+import logo from '../assets/logo.png';
+import cart_icon from '../assets/cart_icon.png';
+
 const navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>Shopper</p>
+      </div>
+      <ul className="nav-menu">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   )
 }
