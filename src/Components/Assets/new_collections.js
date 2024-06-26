f761fee824c47b420adcf21b44e8b0d3140f8da8
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-26 19:27:37 +0530

    added footer section for offers

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -10,7 +10,7 @@ import p8_img from "./product_28.png";
 let new_collections = [
   {
     id: 12,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "Striped Womens Top",
     image: p1_img,
     new_price: 50.0,
     old_price: 80.5,
@@ -45,7 +45,7 @@ let new_collections = [
   },
   {
     id: 2,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    name: "S Flutter Sleeve Overlap Collar Peplum Hem Blouse",
     image: p6_img,
     new_price: 85.0,
     old_price: 120.5,
