a48c22f5149cb6a53331e9ea76a91ae2c524adb9
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-27 15:57:27 +0530

    added product section

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider, { ShopContext } from './Components/Context/ShopContext';
+import ProductDisplay from './Components/ProductDisplay/ProductDisplay';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
