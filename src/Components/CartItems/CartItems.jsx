fdce5285fa80208c209cba924433e5070b920dd4
Aryankpoor <Kapooraryan0419@gmail.com>
2024-06-28 17:35:40 +0530

    added cart price update, encountered totalling error

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import './CartItems.css'
 import { ShopContext } from '../Context/ShopContext';
 import remove_icon from '../Assets/cart_cross_icon.png';
+import { getTotalCartAmount } from '../Context/ShopContext';
 
 const CartItems = () => {
     const {all_product,cartItems,removeFromCart} = useContext(ShopContext);
@@ -29,7 +30,37 @@ const CartItems = () => {
         <hr />
       </div>
        }
+       return null;
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+            <h1>Cart Total</h1>
+            <div>
+                <div className="cartitems-total-item">
+                    <p>Subtotal</p>
+                    <p>₹{getTotalCartAmount()}</p>
+                </div>
+                <hr />
+                <div className="cartitems-total-item">
+                    <p>Shipping Cost</p>
+                    <p>Free</p>
+                </div>
+                <hr />
+                <div className="cartitems-total-item">
+                    <h3>Total</h3>
+                    <h3>₹{getTotalCartAmount()}</h3>
+                </div>
+            </div>
+            <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+            <p>if you have a promocode , Enter it here</p>
+            <div className="cartitems-promobox">
+                <input type="text" placeholder="promo code" />
+                <button>Submit</button>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/Context/ShopContext.jsx
+++ src/Components/Context/ShopContext.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import all_product from "../Assets/all_product";
 import { createContext } from "react";
 
-
 export const ShopContext = createContext(null);
 
 const getDefaultCart = ()=>{
@@ -25,7 +24,20 @@ const ShopContextProvider = (props) => {
         
      }
 
-     const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+     const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems){
+            
+            if(cartItems[item]>0)
+                {
+                    let itemInfo = all_product.find((product)=>product.id===Number(item))
+                    totalAmount += itemInfo.new_price * cartItems[item];
+                }
+                return totalAmount;
+        }
+     }
+
+     const contextValue = {getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
