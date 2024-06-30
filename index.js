6d7d2a6c92051a98b9fc4bf93bfbf171693e5ed4
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-01 00:09:47 +0530

    setup image retreival from server and resolved mongodb auth error

---
+++ index.js
@@ -0,0 +1,55 @@
+const port = 4000;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+const { error } = require('console');
+
+// Connect cors to port  
+app.use(express.json());
+app.use(cors());
+
+// Initialise database connection with mongoDB
+mongoose.connect("mongodb+srv://aryankap:PnQFfx42fOiv6RRA@cluster0.gpo0jdg.mongodb.net/e-commerce");
+
+// API creation
+
+app.get("/", (req,res)=>{
+    res.send("Express App is Running")
+})
+
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null, `${file.fieldname}_${Date.ow()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+// Creating Upload Endpoint for images
+app.use('/images',express.static('upload/images'))
+app.post("/upload",upload.single('product'),(req,res)=>{
+   res.json({
+    success:1,
+    image_url:`http://localhost:${port}/images/${req.file.filename}`
+   })
+})
+
+
+
+app.listen(port,(error)=>{
+    if (!error) {
+      console.log("Server is running on port: "+port)
+    }
+    else 
+    {
+        console.log("Error: "+error)
+    }
+})
+
