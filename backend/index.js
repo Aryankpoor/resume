d3f349c5d4ee25c65d0e0fa057646798ddcf5b92
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-02 22:04:05 +0530

    added add, remove and fetch products api functionality

--- backend/index.js
+++ backend/index.js
@@ -82,9 +82,50 @@ const Product = mongoose.model("Product",{
 
 
 app.post('/addproduct',async (req,res)=>{
+        let products = await Product.find({});
+        let id;
+        if(products.length>0)
+            {
+                let last_product_array = products.slice(-1);
+                let last_product = last_product_array[0];
+                id = last_product.id+1;
+            }
+            else{
+                id=1;
+            }
     const product = new Product({
-        id:req.body
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Product Added Successfully");
+    res.json({
+        success:true,
+        name:req.body.name,
     })
+})
+
+// Creating API for deleting products
+
+app.post('/removeproduct',async (req,res)=>{
+     await Product.findOneAndDelete({id:req.body.id});
+     console.log("Removed");
+     res.json({
+        success:true,
+        name:req.body.name, 
+     })
+})
+
+// Creating API for getting all products
+app.get('/allproducts',async (req,res)=>{
+     let products = await Product.find({});
+     console.log("All Products Fetched");
+     res.send(products);
 })
 
 app.listen(port,(error)=>{
