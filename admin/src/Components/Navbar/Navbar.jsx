18ef67a151b84d9e94b3632e63ae82f10dfb0264
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-03 10:00:31 +0530

    added sidebar and media queries to admin page

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
        <img src={navlogo} alt="" className='nav-logo' />
-       <img src={navProfile} alt="" />
+       <img src={navProfile} className="nav-profile" alt="" />
     </div>
   )
 }
