613f0311fe2876cdb8adaa70078aad48e6fba6a9
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-05 23:52:48 +0530

    added login signup function

--- backend/index.js
+++ backend/index.js
@@ -188,6 +188,22 @@ app.post('/login',async(req,res)=>{
     let user = await Users.findOne({email:req.body.email});
     if (user) {
         const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,'secret_ecom');
+            res.json({success:true,token})
+        }
+        else{
+           res.json({success:false,errors:"Invalid Password"})
+        }
+    }
+    else{
+        res.json({success:false,errors:"Account does not exist with this email"})``
+    
     }
 })
 
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,18 +1,42 @@
 import './CSS/LoginSignup.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginSignup = () => {
+
+  const [state,setState] = useState("Login");
+  const [formData,setFormData] = useState({
+    username:"",
+    password:"",
+    email:""
+  })
+  
+  const changeHandler = (e) => {
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+
+  const login = async () => {
+    console.log("Login Function Executed",formData);
+  }
+
+  const signup = async () => {
+    console.log("Signup Function Executed",formData);
+  }
+
   return (
     <div className='loginsignup'>
 <div className="loginsignup-container">
-  <h1>Sign up</h1>
+  <h1>{state}</h1>
   <div className="loginsignup-fields">
-    <input type="text" placeholder="Your Name" />
-    <input type="email" placeholder="Email Address" />
-    <input type="password" placeholder="Password" />
+    {state==="Sign Up"?<input type="text" name='username' value={formData.username} onChange={changeHandler} placeholder="Your Name" />:<></>}
+    <input type="email" name='email' value={formData.email} onChange={changeHandler} placeholder="Email Address" />
+    <input type="password" name='password' value={formData.password} onChange={changeHandler} placeholder="Password" />
   </div>
-  <button>Continue</button>
-  <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+  <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+  {state==="Sign Up"?
+  <p className='loginsignup-login'>Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p>: 
+   <p className='loginsignup-login'>Create an account <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>}
+  
+ 
   <div className="loginsignup-agree">
     <input type="checkbox" name='' id='' />
     <p>By continuing, I agree to the terms of use & privacy policy</p>
