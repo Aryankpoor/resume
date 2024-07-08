0c3a0e73e40091e47e963ab3120d4c2c35b0bb8d
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-08 09:36:23 +0530

    updated backend url

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
   const [popularProducts,setPopularProducts] = useState([]);
 
   useEffect(()=>{
-   fetch('http://localhost:4000/popularinwomen')
+   fetch('https://secondhand-backend-5b5z.onrender.com/popularinwomen')
    .then((response)=>response.json())
    .then((data)=>setPopularProducts(data));
   },[])
