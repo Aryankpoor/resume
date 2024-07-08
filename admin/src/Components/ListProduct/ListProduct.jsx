92cf504b9728a197415b3d56a794edd41bd32a94
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-08 09:43:14 +0530

    updated admin

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -7,7 +7,7 @@ const ListProduct = () => {
    const [allproducts,setAllProducts] = useState([]);
 
    const fetchInfo = async () => {
-    await fetch('http://localhost:4000/allproducts')
+    await fetch('https://secondhand-backend-5b5z.onrender.com/allproducts')
     .then((res)=>res.json())
     .then((data)=>{setAllProducts(data)});
    }
@@ -17,7 +17,7 @@ const ListProduct = () => {
    },[])
 
    const remove_product = async (id) =>{
-    await fetch('http://localhost:4000/removeproduct', {
+    await fetch('https://secondhand-backend-5b5z.onrender.com/removeproduct', {
       method: 'POST',
       headers: {
         Accept:'application/json',
@@ -45,8 +45,8 @@ const ListProduct = () => {
           return <><div key={index} className="listproduct-format-main listproduct-format">
               <img src={product.image} alt="" className="listproduct-product-icon" />           
               <p>{product.name}</p>
-              <p>${product.old_price}</p>
-              <p>${product.new_price}</p>
+              <p>₹{product.old_price}</p>
+              <p>₹{product.new_price}</p>
               <p>{product.category}</p>
               <img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon' src={cross_icon} alt="" />
           </div>
