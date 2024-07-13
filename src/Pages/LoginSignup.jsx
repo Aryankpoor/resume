68255ceec47293a5d89ee329670bb75f0e2ca9c8
Aryan Kapoor <64773763+Aryankpoor@users.noreply.github.com>
2024-07-13 19:38:39 +0530

    changed error

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -18,7 +18,7 @@ const LoginSignup = () => {
     console.log("Login Function Executed",formData);
     console.log("Signup Function Executed",formData);
     let responseData;
-    await fetch('https://sec-backend.vercel.app/login',{
+    await fetch('http://localhost:4000/login',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
@@ -39,7 +39,7 @@ const LoginSignup = () => {
   const signup = async () => {
     console.log("Signup Function Executed",formData);
     let responseData;
-    await fetch('https://sec-backend.vercel.app/signup',{
+    await fetch('http://localhost:4000/signup',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
