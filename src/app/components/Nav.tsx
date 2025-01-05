04b79eddfe0829ed0a0383257095591ca18a76e9
Aryankpoor <kapooraryan0419@gmail.com>
2025-01-05 18:48:17 +0530

    revamp site to next js and prisma

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,14 @@
+"use client"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ComponentProps, ReactNode } from "react";
+
+export function Nav({ children}: { children: ReactNode}) {
+    return <nav className="bg-primary text-primary-foreground flex justify-center px-4">{children}</nav>
+}
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
+    const pathname = usePathname()
+    return <Link {...props} className={cn()}/>
+}
