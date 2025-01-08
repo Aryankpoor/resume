fb11f87c1ca9de1a184755681a0a68c595f63f6c
Aryankpoor <kapooraryan0419@gmail.com>
2025-01-09 01:12:49 +0530

    schema

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,5 +1,6 @@
 "use client"
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { cn } from "@/lib/utils";
+ 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ComponentProps, ReactNode } from "react";
@@ -10,5 +11,5 @@ export function Nav({ children}: { children: ReactNode}) {
 
 export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
     const pathname = usePathname()
-    return <Link {...props} className={cn()}/>
+    return <Link {...props} className={cn("p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground", pathname === props.href && "bg-background text-foreground")}/>
 }
